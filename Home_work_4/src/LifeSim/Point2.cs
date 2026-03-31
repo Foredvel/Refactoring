043ec53867204faafa3838ca5cbf3ea428aa5f1a
Dmitriy <dhoptyar@list.ru>
2026-03-31 22:37:15 +0700

    Завистливая функция
    Обращение другому объекту, больше чем к своему. Метод рефакторинга: перемещение метода. Предотвращает утекание части семантики

--- Home_work_4/src/LifeSim/Point2.cs
+++ Home_work_4/src/LifeSim/Point2.cs
@@ -2,5 +2,8 @@ namespace LifeSim;
 
 public readonly record struct Point2(int X, int Y)
 {
-    public override string ToString() => $"({X},{Y})";
+    public override string ToString() => $"({X},{Y})"; //тереотическая общность, то нигде не используется 
+
+    public static bool AreNeighborsOrSame(Point2 a, Point2 b) =>
+        Math.Abs(a.X - b.X) <= 1 && Math.Abs(a.Y - b.Y) <= 1;
 }
